de6b21e265485d124a7d9a42cce160c3bbca70e4
MatiasSharofi <129327348+MatiasSharofi@users.noreply.github.com>
2023-12-19 09:56:54 -0500

    Update aiCard.java

--- aiCard.java
+++ aiCard.java
@@ -1,22 +1,43 @@
 public class aiCard {
+	
 	private int health;
 	private int attack;
+	private int coinsDropped;
+	private String name;
 	
-	public aiCard (int health, int attack)
+	public aiCard (int health2, int attack2, int coinsDropped2, String name2)
 	{
-		this.health = health;
-		this.attack = attack;
+		health = health2;
+		attack = attack2;
+		coinsDropped = coinsDropped2;
+		name = name2;
 	}
 	public int getHealth()
 	{
 		return health;
 	}
-	public void setHealth(int aiHealth)
+	public void setHealth(int health1)
 	{
-		health = aiHealth;
+		health = health1;
 	}
 	public int getAttack()
 	{
 		return attack;
+	}
+	public void setAttack(int attack1)
+	{
+		attack = attack1;
+	}
+	public int getCoinsDropped()
+	{
+		return coinsDropped;
+	}
+	public void setCoinsDropped(int coinsDropped2)
+	{
+		coinsDropped = coinsDropped2;
+	}
+	public String getName()
+	{
+		return name;
 	}
 }

f113357d16216df606cb6340f1033c22d7366b05
MatiasSharofi <129327348+MatiasSharofi@users.noreply.github.com>
2024-01-22 21:38:41 -0500

    Update aiCard.java

--- aiCard.java
+++ aiCard.java
@@ -4,40 +4,54 @@ public class aiCard {
 	private int attack;
 	private int coinsDropped;
 	private String name;
+	private int tier;
 	
-	public aiCard (int health2, int attack2, int coinsDropped2, String name2)
+	public aiCard (int health2, int attack2, int coinsDropped2, String name2, int tier2)
 	{
 		health = health2;
 		attack = attack2;
 		coinsDropped = coinsDropped2;
 		name = name2;
+		tier = tier2;
 	}
 	public int getHealth()
 	{
 		return health;
 	}
-	public void setHealth(int health1)
+	public void setHealth(int health3)
 	{
-		health = health1;
+		this.health = health3;
 	}
 	public int getAttack()
 	{
 		return attack;
 	}
-	public void setAttack(int attack1)
+	public void setAttack(int attack3)
 	{
-		attack = attack1;
+		this.attack = attack3;
 	}
 	public int getCoinsDropped()
 	{
 		return coinsDropped;
 	}
-	public void setCoinsDropped(int coinsDropped2)
+	public void setCoinsDropped(int coinsDropped3)
 	{
-		coinsDropped = coinsDropped2;
+		this.coinsDropped = coinsDropped3;
 	}
 	public String getName()
 	{
 		return name;
+	}
+	public void setName (String name3)
+	{
+		this.name = name3;
+	}
+	public int getTier()
+	{
+		return tier;
+	}
+	public void setTier (int tier3)
+	{
+		this.tier = tier3;
 	}
 }
